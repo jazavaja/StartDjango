a349ee37b2b151647a947986c20fca1a8d952ef1
Javad <abtinafraz@gmail.com>
2025-02-06 21:09:30 +0330

    Added 1

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import aboutus, safe_asli_site, get_post, base
+
+urlpatterns = [
+    path('', safe_asli_site , name = 'asli'),
+    path('base', base , name = 'asli'),
+    path('aboutus', aboutus, name = 'ab'),
+    path('post/<int:id>', get_post)
+]
