bcc01cb1686100ac880aaca3fba30c3b1aab3725
Javad <abtinafraz@gmail.com>
2025-02-18 22:40:03 +0330

    Added 1

--- user/models.py
+++ user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.core.validators import EmailValidator
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    family = models.CharField(max_length=100)
+    email = models.EmailField(unique=True,validators=[EmailValidator])
+    age = models.IntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'کاربران'
+        verbose_name = 'کاربر'
