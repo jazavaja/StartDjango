45c0928648d5a925b17970a2953ad8de0ded0b60
Javad <abtinafraz@gmail.com>
2025-02-23 22:35:31 +0330

    4-11 -3  complete

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 
 from store.views import list_category_view, detail_category_view, delete_category_view, update_category_view, \
-    create_category_view
+    create_category_view, product_list_view, product_update_view, product_create_view, \
+    product_delete_view
 
 urlpatterns = [
     path('list_category', list_category_view, name='list_category'),
     path('detail_category_view/<int:id>', detail_category_view, name='detail_category_view'),
-    path('update/<int:id>',update_category_view, name='update_category_view'),
-    path('create_category',create_category_view, name='create_category'),
-    path('delete_category/<int:id>', delete_category_view, name='delete_category')
+    path('update_category/<int:id>', update_category_view, name='update_category_view'),
+    path('create_category', create_category_view, name='create_category'),
+    path('delete_category/<int:id>', delete_category_view, name='delete_category'),
+
+    path('list_products', product_list_view, name='list_products'),
+    path('detail_products_view', product_update_view, name='detail_products_view'),
+    path('update_product/<int:id>', product_update_view, name='update_product'),
+    path('create_product', product_create_view, name='create_product'),
+    path('delete_product/<int:id>', product_delete_view, name='delete_product'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from store.models import Category
+from store.models import Category, Product
 
 
 # Create your views here.
@@ -19,16 +19,15 @@ def create_category_view(request):
     if request.method == 'POST':
         name = request.POST.get('category_name')
         key = request.POST.get('category_key')
-        category = Category.objects.create(name=name,key=key)
+        Category.objects.create(name=name, key=key)
     return render(request, 'create_category.html')
 
 
-
-def update_category_view(request,id):
+def update_category_view(request, id):
     category = Category.objects.get(id=id)
     if request.method == 'POST':
-        category.name = request.POST.get('category_name',category.name)
-        category.key = request.POST.get('category_key',category.key)
+        category.name = request.POST.get('category_name', category.name)
+        category.key = request.POST.get('category_key', category.key)
         category.save()
 
     return render(request, 'view_category.html', {'category': category})
@@ -43,20 +42,42 @@ def delete_category_view(request, id):
 # ----------------------------------------------------
 
 def product_list_view(request):
-    pass
+    search = request.GET.get('search_input')
+    if search:
+        products = Product.objects.filter(name__contains=search)
+    else:
+        products = Product.objects.all()
 
-
-def product_detail_view(request):
-    pass
+    return render(request, 'product_list_view.html', {'products': products})
 
 
 def product_create_view(request):
-    pass
+    if request.method == "POST":
+        product_name = request.POST.get('product_name')
+        product_price = request.POST.get('product_price')
+        product_category = request.POST.get('product_category')
+
+        print(f"Product name {product_name} .. price {product_price} ... category {product_category}")
+        product = Product(name=product_name, price=product_price, category_id=product_category)
+        product.save()
+
+    category = Category.objects.all()
+    return render(request, 'product_create.html', {'category': category})
 
 
-def product_update_view(request):
-    pass
+def product_update_view(request, id):
+    category = Category.objects.all()
+    p = Product.objects.get(id=id)
+    if request.method == "POST":
+        p.name = request.POST.get('product_name')
+        p.price = request.POST.get('product_price')
+        p.category_id = request.POST.get('product_category')
+        p.save()
+
+    return render(request, 'product_update_view.html', {'product': p, 'category': category})
 
 
-def product_delete_view(request):
-    pass
+def product_delete_view(request, id):
+    p = Product.objects.get(id=id)
+    p.delete()
+    return redirect('list_products')
