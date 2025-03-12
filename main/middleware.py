cfea99311f8b12be07daae62b9adc369107c3843
Javad <abtinafraz@gmail.com>
2025-03-12 15:47:53 +0330

    8-4 Complete

--- main/middleware.py
+++ main/middleware.py
@@ -1,5 +1,7 @@
 import json
 from datetime import datetime
+from django.http import HttpResponseForbidden
+import requests
 
 
 class RequestLoggingMiddleware:
@@ -37,3 +39,39 @@ class RequestLoggingMiddleware:
         logs.append(log_data)
         with open(file_name, 'w', encoding='utf-8') as log_file:
             json.dump(logs, log_file, indent=2)
+
+
+class BlockIpMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.ip_list = []
+
+    def get_ip(self, request):
+        http_x = request.META.get('HTTP_X_FORWARDED_FOR')
+        if http_x:
+            ip = http_x.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
+    def list_country_block(self):
+        block_list = [
+            'SE', 'IT', 'DE'
+        ]
+        return block_list
+
+    def __call__(self, request):
+        # my_ip = self.get_ip(request)
+        my_ip = '2.56.28.0'
+        url = f'https://ipinfo.io/{my_ip}?token=77ffa2ec464e8f'
+        try:
+            result = requests.get(url)
+            json_result = result.json()
+            print(json_result['country'])
+            if json_result['country'] in self.list_country_block():
+                return HttpResponseForbidden("Access to the site for users in Italy, Sweden, and Germany is banned.")
+
+        except:
+            print('We can not request get from url ipinfo')
+        response = self.get_response(request)
+        return response
